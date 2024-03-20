ab1a577f89c3ed2df95fc47e8fb66d7f70c20b25
Bobby Stocker <bobby@starcomsys.net>
2024-03-20 11:03:09 -0700

    Bug fix

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -3,7 +3,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-import Svg2 from "../Svg/svg-3";
 
 const formSchema = z.object({
   name: z.string().min(2, {
